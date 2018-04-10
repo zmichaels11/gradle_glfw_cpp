484a56763cf0da4ae56fd15c93626c0d860c5802
zmichaels <zmichaels11@gmail.com>
2018-04-10 15:31:22 -0700

    Add GLEW init to hello_world - add GLEW Init - add print statements to confirm OpenGL initialized

--- src/hello_world/cpp/main.cpp
+++ src/hello_world/cpp/main.cpp
@@ -17,6 +17,21 @@ int main(int argc, char ** argv) {
 
     glfwMakeContextCurrent(window);
 
+    {
+        auto err = glewInit();
+
+        if (GLEW_OK != err) {
+            auto errmsg = glewGetErrorString(err);
+
+            throw std::runtime_error(reinterpret_cast<const char * > (errmsg));
+        }
+    }
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION)
+        << "\nOpenGL Renderer: " << glGetString(GL_RENDERER)
+        << "\nOpenGL Shading Language Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION)
+        << std::endl;
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
